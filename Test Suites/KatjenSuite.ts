c9000cec0a635c2e60823b5ee9ed8c6622d866e8
anuja.jagtap@apmosys.mahape <anuja.jagtap@apmosys.mahape@APMOSYSLT0502>
2024-09-06 18:25:23 +0530

    First Push

---
+++ Test Suites/KatjenSuite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>KatjenSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>186da00b-4408-49c0-81c1-ff9cb00b5337</testSuiteGuid>
+   <testCaseLink>
+      <guid>612b79d0-512b-4256-80f2-de512ec94cb4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/KatJen1</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
